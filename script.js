f05c41f5284a67b7cd8640b955dde71edbeb7d3c
fredrika-g <fredrika.gerring@gmail.com>
2023-12-02 13:21:25 +0100

    functionality to check answers and score

--- script.js
+++ script.js
@@ -2,92 +2,92 @@ const questions = [
   {
     q: "This is either true or false",
     a: [
-      { text: "Sant", isCorrect: true },
-      { text: "Falskt", isCorrect: false },
+      { text: "Sant", value: "sant", isCorrect: true },
+      { text: "Falskt", value: "falskt", isCorrect: false },
     ],
     type: "trueOrFalse",
   },
   {
     q: "Second true/false question",
     a: [
-      { text: "Sant", isCorrect: true },
-      { text: "Falskt", isCorrect: false },
+      { text: "Sant", value: "sant", isCorrect: true },
+      { text: "Falskt", value: "falskt", isCorrect: false },
     ],
     type: "trueOrFalse",
   },
   {
     q: "Third true/false question",
     a: [
-      { text: "Sant", isCorrect: false },
-      { text: "Falskt", isCorrect: true },
+      { text: "Sant", value: "sant", isCorrect: false },
+      { text: "Falskt", value: "falskt", isCorrect: true },
     ],
     type: "trueOrFalse",
   },
   {
     q: "Fourth true/false question",
     a: [
-      { text: "Sant", isCorrect: true },
-      { text: "Falskt", isCorrect: false },
+      { text: "Sant", value: "sant", isCorrect: true },
+      { text: "Falskt", value: "falskt", isCorrect: false },
     ],
     type: "trueOrFalse",
   },
   {
     q: "Vad heter jag?",
     a: [
-      { text: "Sara", isCorrect: false },
-      { text: "Fredrika", isCorrect: true },
-      { text: "Märta", isCorrect: false },
-      { text: "Gabriella", isCorrect: false },
+      { text: "Sara", value: "sara", isCorrect: false },
+      { text: "Fredrika", value: "fredrika", isCorrect: true },
+      { text: "Märta", value: "märta", isCorrect: false },
+      { text: "Gabriella", value: "gabriella", isCorrect: false },
     ],
     type: "multipleChoice",
   },
   {
     q: "Hur gammal är jag?",
     a: [
-      { text: "25", isCorrect: true },
-      { text: "23", isCorrect: false },
-      { text: "24", isCorrect: false },
-      { text: "27", isCorrect: false },
+      { text: "25", value: "25", isCorrect: true },
+      { text: "23", value: "23", isCorrect: false },
+      { text: "24", value: "24", isCorrect: false },
+      { text: "27", value: "27", isCorrect: false },
     ],
     type: "multipleChoice",
   },
   {
     q: "Svara rätt",
     a: [
-      { text: "Svar A", isCorrect: false },
-      { text: "Svar B", isCorrect: false },
-      { text: "Svar C", isCorrect: true },
-      { text: "Svar C", isCorrect: false },
+      { text: "Svar A", value: "A", isCorrect: false },
+      { text: "Svar B", value: "B", isCorrect: false },
+      { text: "Svar C", value: "C", isCorrect: true },
+      { text: "Svar D", value: "D", isCorrect: false },
     ],
     type: "multipleChoice",
   },
   {
     q: "Vad heter jag i mellannamn? Två rätta svar",
     a: [
-      { text: "Elisabeth", isCorrect: false },
-      { text: "Helena", isCorrect: true },
-      { text: "Kristina", isCorrect: true },
-      { text: "Ingrid", isCorrect: false },
+      { text: "Elisabeth", value: "elisabeth", isCorrect: false },
+      { text: "Helena", value: "helena", isCorrect: true },
+      { text: "Kristina", value: "kristina", isCorrect: true },
+      { text: "Ingrid", value: "ingrid", isCorrect: false },
     ],
     type: "checkbox",
   },
   {
     q: "Fråga här? Tre rätta svar",
     a: [
-      { text: "Alt 1", isCorrect: true },
-      { text: "Alt 2", isCorrect: false },
-      { text: "Alt 3", isCorrect: true },
-      { text: "Alt 4", isCorrect: false },
+      { text: "Alt 1", value: "1", isCorrect: true },
+      { text: "Alt 2", value: "2", isCorrect: true },
+      { text: "Alt 3", value: "3", isCorrect: true },
+      { text: "Alt 4", value: "4", isCorrect: false },
     ],
     type: "checkbox",
   },
   {
     q: "En till fråga här? Två rätta svar",
     a: [
-      { text: "Alt 1", isCorrect: false },
-      { text: "Alt 2", isCorrect: true },
-      { text: "Alt 3", isCorrect: false },
-      { text: "Alt 4", isCorrect: true },
+      { text: "Alt 1", value: "1", isCorrect: false },
+      { text: "Alt 2", value: "2", isCorrect: true },
+      { text: "Alt 3", value: "3", isCorrect: false },
+      { text: "Alt 4", value: "4", isCorrect: true },
     ],
     type: "checkbox",
   },
@@ -118,7 +118,7 @@ const submitQuiz = () => {
   let warningMsg = document.querySelector(".warning");
   warningMsg.innerHTML = "";
   if (allAnswered()) {
-    console.log("all answered");
+    console.log(checkAnswers());
   } else {
     warningMsg.innerText = "Du måste svara på alla frågor!";
   }
@@ -155,12 +155,13 @@ const buildRadioQuestion = (q, index) => {
     let aDiv = document.createElement("div");
     let label = document.createElement("label");
     let answer = q.a[i].text;
+    let value = q.a[i].value;
     label.innerText = answer;
-    label.setAttribute("for", `${answer}${index}`);
+    label.setAttribute("for", `${value}${index}`);
     let radioBtn = document.createElement("input");
     radioBtn.setAttribute("type", "radio");
-    radioBtn.setAttribute("value", answer);
-    radioBtn.setAttribute("id", `${answer}${index}`);
+    radioBtn.setAttribute("value", value);
+    radioBtn.setAttribute("id", `${value}${index}`);
     radioBtn.setAttribute("name", `radio${index}`);
     aDiv.append(radioBtn, label);
     div.append(aDiv);
@@ -181,12 +182,13 @@ const buildCheckbox = (q, index) => {
     let aDiv = document.createElement("div");
     let label = document.createElement("label");
     let answer = q.a[i].text;
+    let value = q.a[i].value;
     label.innerText = answer;
-    label.setAttribute("for", `${answer}${index}`);
+    label.setAttribute("for", `${value}${index}`);
     let checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("value", answer);
-    checkbox.setAttribute("id", `${answer}${index}`);
+    checkbox.setAttribute("value", value);
+    checkbox.setAttribute("id", `${value}${index}`);
     checkbox.setAttribute("name", `box${index}`);
     aDiv.append(checkbox, label);
     div.append(aDiv);
@@ -212,10 +214,65 @@ const allAnswered = () => {
 };
 
 const checkAnswers = () => {
-  let checkedRadios = document.querySelectorAll("[type='radio']:checked");
-  let checkedBoxes = document.querySelectorAll("[type='checkbox']:checked");
-
   let score = 0;
+  let compiledAnswers = [];
+  let isCorrect = false;
+  let allChecked = [];
+  let answers = [];
 
-  questions.forEach((question) => {});
+  let inputElements = document.querySelectorAll("#quizContent input");
+
+  questions.forEach((question) => {
+    let inputs = [...inputElements].filter((input) => {
+      if (questionMatchesAnswer(input, question)) {
+        return input.checked;
+      }
+    });
+
+    let everyCorrect = question.a.filter((a) => {
+      return a.isCorrect;
+    });
+
+    if (inputs.length === everyCorrect.length) {
+      for (i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === everyCorrect[i].value) {
+          isCorrect = true;
+          answers.push(everyCorrect[i].text);
+        }
+        allChecked.push(everyCorrect[i].text);
+      }
+
+      if (isCorrect && answers.length === everyCorrect.length) {
+        score++;
+        compiledAnswers.push({ answer: answers, isCorrect: true });
+      } else {
+        let wrong = {};
+        if (question.type === "checkbox") {
+          wrong = {
+            answer: "Du valde fel alternativ!",
+            isCorrect: false,
+          };
+          compiledAnswers.push(wrong);
+        } else {
+          wrong = { answer: allChecked, isCorrect: false };
+          compiledAnswers.push(wrong);
+        }
+      }
+    } else {
+      compiledAnswers.push({
+        answer: `Du valde ${inputs.length} alternativ, det finns ${everyCorrect.length} korrekta svar!`,
+        isCorrect: false,
+      });
+    }
+  });
+
+  return { answers: compiledAnswers, score: score };
+};
+
+// check if answer and question belong together
+const questionMatchesAnswer = (input, question) => {
+  return (
+    +input.parentElement.parentElement.dataset.id ===
+    questions.indexOf(question)
+  );
 };
